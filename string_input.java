8734514a82ed499c6e57ef0c0ec06f9f174d5579
SAI RAM <79187951+stu1219@users.noreply.github.com>
2021-02-17 12:39:22 +0530

    Create string_input.java

---
+++ string_input.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class stringinput {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+		System.out.println("intrest "+ s);
+	}
+}
